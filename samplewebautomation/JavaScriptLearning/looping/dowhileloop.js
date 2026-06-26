b68bbc6d8eb2506c7278498072f9a9f84555ce6a
Getupandsleep <rangnath.t.p@gmail.com>
2026-06-26 09:03:02 +0530

    Looping statemnts

---
+++ samplewebautomation/JavaScriptLearning/looping/dowhileloop.js
@@ -0,0 +1,12 @@
+let i=10
+do{
+    console.log(i);
+    i++
+}while(i<=20)
+ 
+
+let j=10
+do{
+    console.log(j)
+    j--
+}while(j>=1)
---
+++ samplewebautomation/JavaScriptLearning/looping/forloop.js
@@ -0,0 +1,17 @@
+// print numbers 10 to 20
+for(let i=10;i<=20;i++){
+    console.log(i);
+}
+ 
+
+ 
+let i=5
+for(;i<=15;){
+    console.log(i);
+    i++
+}
+ 
+
+for(let i=10;i>=1;i--){
+    console.log(i);
+}
